c3fc6c917a68ddca48dbd448cd1b02ccb3f1a6bc
yeondub1121 <kssjshyjj@naver.com>
2024-11-07 14:25:44 +0900

    6주차_미션

---
+++ src/main/generated/umc/spring/study/domain/QReview.java
@@ -0,0 +1,41 @@
+package umc.spring.study.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = -1655301601L;
+
+    public static final QReview review = new QReview("review");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Float> score = createNumber("score", Float.class);
+
+    public final StringPath title = createString("title");
+
+    public QReview(String variable) {
+        super(Review.class, forVariable(variable));
+    }
+
+    public QReview(Path<? extends Review> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReview(PathMetadata metadata) {
+        super(Review.class, metadata);
+    }
+
+}
+
